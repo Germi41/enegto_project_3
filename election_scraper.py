3e5058b19e3d651256536fe74bb90d166014b654
david.germ <david.germ@kiwi.com>
2023-03-04 15:03:54 +0100

    feature: collect data function

--- election_scraper.py
+++ election_scraper.py
@@ -4,8 +4,10 @@ author: David Germ
 email: david.germ@kiwi.com
 discord: GermiCZ#2828
 """
+import unicodedata
 
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 
@@ -13,43 +15,82 @@ def main():
     base_url = "https://www.volby.cz/pls/ps2017nss/"
     url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=5&xnumnuts=4101"
     soup = get_response(url)
-    links_table = get_municipality_links(soup, base_url)
-    print(type(links_table))
-    get_data(links_table)
+    results = get_municipality_links(soup, base_url)
+    # print(results)
 
 
 def get_response(url):
     response = requests.get(url)
     return BeautifulSoup(response.text, "html.parser")
 
+
 def get_municipality_links(soup, base_url):
-    url = []
-    table = soup.find_all('td', {'class': 'center'})
-    for each_td in table:
+    results = []
+    for each_td in soup.find_all('td', {'class': 'cislo'}):
         line = each_td.a['href']
-        url.append(base_url + line)
-    return url
+        url = base_url + line
+        line2 = [each_td.text]
+        line2.extend(get_data(url))
+        results.append(line2)
+
+    print(*results, sep="\n")
+    return results
 
 
-def get_data(links_table: list):
-    # print(links_table)
-    i = 0
-    for link in links_table:
-        response = requests.get(link)
-        soup = BeautifulSoup(response.text, "html.parser")
-        collect_data(soup)
-        # print(*soup, sep="\n")
+def get_data(links_table):
+    response = requests.get(links_table)
+    soup = BeautifulSoup(response.text, "html.parser")
+    data = collect_votes(soup)
+
+    return data
 
 
-def collect_data(soup):
-    name = []
-    table = soup.find_all('td', {'class': 'overflow_name'})
-    for each_td in table:
-        name.append(each_td.text)
+def collect_votes(soup):
+    data = []
+    registered = soup.find('td', {'headers': 'sa2'}).text
+    envelopes = soup.find('td', {'headers': 'sa5'}).text
+    valid = soup.find('td', {'headers': 'sa6'}).text
 
-    print(*name, sep="\n")
+    if "\xa0" in registered:
+        clean_reg = unicodedata.normalize("NFKD", registered)
+        data.append(clean_reg)
+    else:
+        data.append(registered)
+
+    if "\xa0" in envelopes:
+        clean_env = unicodedata.normalize("NFKD", envelopes)
+        data.append(clean_env)
+    else:
+        data.append(envelopes)
+
+    if "\xa0" in valid:
+        clean_val = unicodedata.normalize("NFKD", valid)
+        data.append(clean_val)
+    else:
+        data.append(valid)
+
+    return data
 
 
+# def save_to_csv(tabulka: list, file: str):
+#     """Uloz tabulku men s datem do CSV souboru"""
+#     header = ["Code", "Location", "Registered", "Envelopes",
+#                "Valid", "Občanská demokratická strana",
+#                "Řád národa - Vlastenecká unie", "CESTA ODPOVĚDNÉ SPOLEČNOSTI",
+#                "Česká str.sociálně demokrat.", "Radostné Česko",
+#                "STAROSTOVÉ A NEZÁVISLÍ", "Komunistická str.Čech a Moravy",
+#                "Strana zelených", "ROZUMNÍ-stop migraci,diktát.EU",
+#                "Strana svobodných občanů", "Blok proti islam.-Obran.domova",
+#                "Občanská demokratická aliance", "Česká pirátská strana",
+#                "Referendum o Evropské unii", "TOP 09",
+#                "ANO 2011", "SPR-Republ.str.Čsl. M.Sládka",
+#                "Křesť.demokr.unie-Čs.str.lid.", "Česká strana národně sociální",
+#                "REALISTÉ", "SPORTOVCI", "Dělnic.str.sociální spravedl.",
+#                "Svob.a př.dem.-T.Okamura (SPD)", "Strana Práv Občanů"]
+#     with open(file, "w", encoding="utf-8", newline="") as csv_s:
+#         write = csv.writer(csv_s, dialect="excel")
+#         write.writerow(header)
+#         write.writerows(tabulka)
 
 
 if __name__ == "__main__":
